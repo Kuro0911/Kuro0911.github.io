28c73e19ca4db25f62eda5fd16e3d222783a7a55
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-07-10 19:17:08 +0530

    added shiba project

--- src/components/Projects/carousel/Carousel.jsx
+++ src/components/Projects/carousel/Carousel.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Wrapper from "./Carousel.style";
+import Shiba from "./images/shiba.png";
+import Image from "mui-image";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LaunchIcon from "@mui/icons-material/Launch";
 
 export const CarouselModal = () => {
   const [index, setIndex] = useState(0);
@@ -18,18 +22,46 @@ export const CarouselModal = () => {
         className="wrap"
       >
         <Carousel.Item>
-          <div className="heading">
-            <text>help</text>
-            <br />
-            <text>help</text>
-            <br />
-            <text>help</text>
-            <br />
+          <div className="container">
+            <div className="left">
+              <Image src={Shiba} width={500} height={400} duration={0} />
+            </div>
+            <div className="right">
+              <div className="heading">
+                <text>Shiba The crypto bot</text>
+              </div>
+              <br />
+              <div className="about">
+                <p>
+                  Do we make money or does money make us? Chezwich. I have never
+                  known a Jack that was in good enough shape to name
+                  bodybuilding after him. Do we make money or does money make
+                  us? Chezwich. This is a true fact: I never had a fear of
+                  heights until I fell off a roof. This is a true fact: I never
+                  had a fear of heights until I fell off a roof.{" "}
+                </p>
+              </div>
+              <br />
+              <div className="tags">
+                <span>CryptoCurrency</span>
+                <span>JavaSript</span>
+                <span>ReactJS</span>
+                <span>ChatBot</span>
+              </div>
+              <br />
+              <div className="gotoContainer">
+                <a className="goto" href="https://github.com/Kuro0911">
+                  <GitHubIcon className="icon" />
+                </a>
+                <a
+                  className="goto"
+                  href="https://kuro0911.github.io/Shiba---A-chat-Bot/"
+                >
+                  <LaunchIcon className="icon" />
+                </a>
+              </div>
+            </div>
           </div>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <div className="heading">
--- src/components/Projects/carousel/Carousel.style.js
+++ src/components/Projects/carousel/Carousel.style.js
@@ -4,10 +4,67 @@ const Wrapper = styled.div`
   .wrap {
     width: 70vw;
     height: 60vh;
+    .carousel-inner {
+      min-height: 60vh;
+      .carousel-item {
+        min-height: 60vh;
+      }
+    }
+  }
+  .container {
+    display: flex;
+  }
+  .left {
+    width: 50%;
+    margin-right: 3%;
+  }
+  .right {
+    width: 50%;
   }
   .heading {
-    font-size: xxx-large;
-    color: white;
+    text-align: left;
+    text {
+      font-size: xx-large;
+      font-family: monospace !important;
+    }
+    color: #ccd6f6;
+  }
+  .heading:hover {
+    color: #62fad6;
+  }
+  .about {
+    text-align: left;
+    p {
+      font-family: "Calibre", "Inter", "San Francisco", "SF Pro Text",
+        -apple-system, system-ui, sans-serif;
+      font-size: medium;
+    }
+    color: #8892b0;
+  }
+  .tags {
+    text-align: left;
+    span {
+      font-size: 14px;
+      font-family: monospace !important;
+      margin-right: 1em;
+    }
+    color: #ccd6f6;
+  }
+  .gotoContainer {
+    text-align: left;
+  }
+  .goto {
+    margin-right: 1em;
+    .icon {
+      color: #ccd6f6;
+      font-size: 25px;
+      transition: 0.1s ease-in;
+    }
+    .icon:hover {
+      cursor: pointer;
+      color: #64ffda;
+    }
   }
 `;
+
 export default Wrapper;
