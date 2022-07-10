948715408ae105656490569d1651515d74ffdd2b
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-07-11 04:04:40 +0530

    init

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -2,17 +2,23 @@ import React, { useState } from "react";
 import WorkWrapper, { Container, JpWrapper } from "./work.style";
 
 export const Work = () => {
-  const [text, setText] = useState("Experience");
-  const handleChange = () => {
-    text === "エクスペリエンス"
-      ? setText("Experience")
-      : setText("エクスペリエンス");
+  const [text1, setText1] = useState("Experience");
+  const [text2, setText2] = useState("Skills");
+  const handleChange1 = () => {
+    text1 === "エクスペリエンス"
+      ? setText1("Experience")
+      : setText1("エクスペリエンス");
+  };
+  const handleChange2 = () => {
+    text2 === "スキル" ? setText2("Skills") : setText2("スキル");
   };
   return (
     <WorkWrapper id="work">
       <Container>
-        <JpWrapper text={text}>
-          <text onPointerEnter={handleChange}>{text}</text>
+        <JpWrapper text={text1}>
+          <text onPointerEnter={handleChange1}>{text1}</text>
+          <text> / </text>
+          <text onPointerEnter={handleChange2}>{text2}</text>
         </JpWrapper>
       </Container>
     </WorkWrapper>
