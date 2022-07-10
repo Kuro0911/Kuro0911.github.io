0dae0ce2fcb4a788152539e217a03c059dbefbd5
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-07-10 19:31:42 +0530

    added projects

--- src/components/Projects/carousel/Carousel.jsx
+++ src/components/Projects/carousel/Carousel.jsx
@@ -3,6 +3,8 @@ import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Wrapper from "./Carousel.style";
 import Shiba from "./images/shiba.png";
+import Sort from "./images/sort.png";
+import Netflix from "./images/netflix.png";
 import Image from "mui-image";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LaunchIcon from "@mui/icons-material/Launch";
@@ -50,7 +52,10 @@ export const CarouselModal = () => {
               </div>
               <br />
               <div className="gotoContainer">
-                <a className="goto" href="https://github.com/Kuro0911">
+                <a
+                  className="goto"
+                  href="https://github.com/Kuro0911/Shiba---A-chat-Bot"
+                >
                   <GitHubIcon className="icon" />
                 </a>
                 <a
@@ -64,34 +69,94 @@ export const CarouselModal = () => {
           </div>
         </Carousel.Item>
         <Carousel.Item>
-          <div className="heading">
-            <text>help</text>
-            <br />
-            <text>help</text>
-            <br />
-            <text>help</text>
-            <br />
+          <div className="container">
+            <div className="left">
+              <Image src={Sort} width={500} height={400} duration={0} />
+            </div>
+            <div className="right">
+              <div className="heading">
+                <text>SortViz</text>
+              </div>
+              <br />
+              <div className="about">
+                <p>
+                  Do we make money or does money make us? Chezwich. I have never
+                  known a Jack that was in good enough shape to name
+                  bodybuilding after him. Do we make money or does money make
+                  us? Chezwich. This is a true fact: I never had a fear of
+                  heights until I fell off a roof. This is a true fact: I never
+                  had a fear of heights until I fell off a roof.{" "}
+                </p>
+              </div>
+              <br />
+              <div className="tags">
+                <span>CryptoCurrency</span>
+                <span>JavaSript</span>
+                <span>ReactJS</span>
+                <span>ChatBot</span>
+              </div>
+              <br />
+              <div className="gotoContainer">
+                <a
+                  className="goto"
+                  href="https://github.com/Kuro0911/Sorting-Visualizer"
+                >
+                  <GitHubIcon className="icon" />
+                </a>
+                <a
+                  className="goto"
+                  href="https://relaxed-jelly-706d87.netlify.app/"
+                >
+                  <LaunchIcon className="icon" />
+                </a>
+              </div>
+            </div>
           </div>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <div className="heading">
-            <text>help</text>
-            <br />
-            <text>help</text>
-            <br />
-            <text>help</text>
-            <br />
+          <div className="container">
+            <div className="left">
+              <Image src={Netflix} width={500} height={400} duration={0} />
+            </div>
+            <div className="right">
+              <div className="heading">
+                <text>Netflix Clone</text>
+              </div>
+              <br />
+              <div className="about">
+                <p>
+                  Do we make money or does money make us? Chezwich. I have never
+                  known a Jack that was in good enough shape to name
+                  bodybuilding after him. Do we make money or does money make
+                  us? Chezwich. This is a true fact: I never had a fear of
+                  heights until I fell off a roof. This is a true fact: I never
+                  had a fear of heights until I fell off a roof.{" "}
+                </p>
+              </div>
+              <br />
+              <div className="tags">
+                <span>CryptoCurrency</span>
+                <span>JavaSript</span>
+                <span>ReactJS</span>
+                <span>ChatBot</span>
+              </div>
+              <br />
+              <div className="gotoContainer">
+                <a
+                  className="goto"
+                  href="https://github.com/Kuro0911/netflix-clone"
+                >
+                  <GitHubIcon className="icon" />
+                </a>
+                <a
+                  className="goto"
+                  href="https://kuro0911.github.io/netflix-clone/"
+                >
+                  <LaunchIcon className="icon" />
+                </a>
+              </div>
+            </div>
           </div>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </Wrapper>
