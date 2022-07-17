c2336e2317ca3dd52addf4974d07cd263774f00a
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-07-18 02:39:32 +0530

    added about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,6 +3,7 @@ import AboutWrapper, {
   Container,
   ImageContainer,
   JpWrapper,
+  TextContainer,
   TextWrapper,
 } from "./about.style";
 import profile from "./utils/profile.jpg";
@@ -19,10 +20,48 @@ export const About = () => {
         <JpWrapper text={text}>
           <text onPointerEnter={handleChange}>{text}</text>
         </JpWrapper>
-        <ImageContainer>
-          <Image src={profile} width={300} height={300} />
-        </ImageContainer>
-        <TextWrapper></TextWrapper>
+        <TextWrapper>
+          <TextContainer>
+            <p>
+              Heyy everyone!! My name is <code>Dhananjai</code> and i like to
+              code.
+            </p>
+            <p>
+              {" "}
+              Okay so to be precise i like bulding things, solving problems and
+              staring at my computer screen for 10 hours a day trying to figure
+              out why is a div not centered AAAA!!!
+              <p />
+              <p>
+                and yes I do code a bit well lets just say alot. I always wanted
+                to create things and looking at things on the web just made my 8
+                year old brain think that how is this working then one fine day
+                i stumbled across <code>HTML & CSS</code> and when i got the
+                tools all I had to do was start and so I did
+              </p>
+              <p>
+                Also <code>Algorithms</code> do interest me alot when I learned
+                about <code>Data Structures & Algorithms</code> a whole new
+                field of Computer Science opened up infront of me from where i
+                started learing more about algorithms and got into problem
+                solving
+              </p>
+              <p>
+                So yes it has been a fun ride and i expect it to get even better
+                where i can learn more and explore
+              </p>
+              <p>
+                Oh and did i mention that I am currently Junior at SRM Institute
+                Of Science and Technology also i like the japanese culture and
+                anime
+              </p>
+              <span>どぞ よろしく</span>
+            </p>
+          </TextContainer>
+          <ImageContainer>
+            <Image src={profile} width={300} height={300} />
+          </ImageContainer>
+        </TextWrapper>
       </Container>
     </AboutWrapper>
   );
--- src/components/about/about.style.js
+++ src/components/about/about.style.js
@@ -6,18 +6,33 @@ const AboutWrapper = styled.div`
   background-color: #09192f;
 `;
 export const Container = styled.div`
-  margin-top: 2%;
-  width: 50%;
+  margin-top: 3%;
+  width: 60%;
   text-align: center;
 `;
+export const TextWrapper = styled.div`
+  margin-top: 5%;
+  display: flex;
+  justify-content: space-between;
+`;
 export const ImageContainer = styled.div`
+  width: 35%;
   .mui-image-wrapper {
     .css-jlmoib {
       border-radius: 100%;
     }
   }
 `;
-export const TextWrapper = styled.div``;
+export const TextContainer = styled.div`
+  width: 65%;
+  color: #ccd6f6;
+  text-align: left;
+  font-size: medium;
+  code {
+    color: #64ffda;
+  }
+`;
+
 export const JpWrapper = styled.div`
   margin-bottom: ${(props) => (props.text === "コンタクト" ? "-1.50%" : "0")};
   text {
