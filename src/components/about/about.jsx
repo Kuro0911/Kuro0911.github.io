0a2e141c377ba40265e3be40e7e05adde0ebd9e7
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-07-19 00:24:36 +0530

    about section finished

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -35,36 +35,29 @@ export const About = () => {
               {" "}
               Okay so to be precise i like bulding things, solving problems and
               staring at my computer screen for 10 hours a day trying to figure
-              out why is a div not centered AAAA!!!
+              out why is a div not centered AAAAHHH!!!
               <p />
               <p>
-                and yes I do code a bit well lets just say alot. I always wanted
-                to create things and looking at things on the web just made my 8
-                year old brain think that how is this working then one fine day
-                i stumbled across <code>HTML & CSS</code> and when i got the
-                tools all I had to do was start and so I did
-              </p>
-              <p>
-                Also <code>Algorithms</code> do interest me alot when I learned
-                about <code>Data Structures & Algorithms</code> a whole new
-                field of Computer Science opened up infront of me from where i
-                started learing more about algorithms and got into problem
-                solving
+                and yes I do code alot. I always wanted to create things and
+                looking at things on the web just made my 8 year old brain think
+                that how is this working then one fine day i stumbled across{" "}
+                <code>HTML & CSS</code> and when i got the tools all I had to do
+                was start and so, I did. Also <code>Algorithms</code> do
+                interest me alot which got me into problem solving using{" "}
+                <code>C++</code>
               </p>
               <p>
                 So yes it has been a fun ride and i expect it to get even better
                 where i can learn more and explore
               </p>
-              <p>
-                Oh and did i mention that I am currently Junior at SRM Institute
-                Of Science and Technology also i like the japanese culture and
-                anime
-              </p>
-              <span>どぞ よろしく</span>
+              <br />
+              <span>どうぞよろしく</span>
             </p>
           </TextContainer>
           <ImageContainer>
-            <Image src={profile} width={270} height={270} />
+            <div className="hover-effect" style={{ "--clr": "#64ffda" }}>
+              <Image src={profile} width={250} height={250} />
+            </div>
             <TagContainer tag={tag}>
               <h3>{name}</h3>
               <h4 onMouseEnter={() => setTag(tag === "黒." ? "kuro." : "黒.")}>
--- src/components/about/about.style.js
+++ src/components/about/about.style.js
@@ -16,29 +16,45 @@ export const TextWrapper = styled.div`
   justify-content: space-between;
 `;
 export const ImageContainer = styled.div`
-  width: 30%;
+  width: fit-content;
   .mui-image-wrapper {
     .css-jlmoib {
       border-radius: 100%;
     }
   }
+  .hover-effect {
+    width: 250px;
+    height: 250px;
+    border-radius: 100%;
+    transition: 0.5s;
+    margin-left: 20%;
+    margin-top: 5%;
+  }
+  .hover-effect:hover {
+    box-shadow: 0 0 16px var(--clr);
+  }
 `;
 export const TagContainer = styled.div`
   text-align: right;
   color: #ccd6f6;
+  margin-top: 5%;
+  display: flex;
+  flex-direction: column;
+  align-items: end;
   h4 {
     color: ${(props) => (props.tag === "黒." ? "#64ffda" : "#ccd6f6")};
   }
   margin-top: 5%;
   p {
     color: #8892b0;
+    width: 81%;
   }
 `;
 export const TextContainer = styled.div`
-  width: 66%;
+  width: 130%;
   color: #ccd6f6;
   text-align: left;
-  font-size: medium;
+  font-size: large;
   code {
     color: #64ffda;
   }
