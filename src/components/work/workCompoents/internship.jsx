dfbc8c135ad8d4d4df9bd1b772c76aa3ca91d006
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-12-10 02:13:14 +0530

    minor changes

--- src/components/work/workCompoents/internship.jsx
+++ src/components/work/workCompoents/internship.jsx
@@ -12,46 +12,76 @@ export const Internship = () => {
       borderColor: "#64ffda",
     },
   };
+  const data = [
+    {
+      positon: "Web Developer Intern",
+      company: "NoteString",
+      duration: "October 2022 - November 2022",
+      location: "San Francisco, USA",
+      resposibilities: [
+        {
+          data: "Performed development tasks, including software design and coding",
+        },
+        {
+          data: "Wrote modular/reusable/object-oriented Javascript code and used frameworks like Svelte, SvelteKit, etc",
+        },
+        {
+          data: "Documented application changes and developing updates",
+        },
+      ],
+      companyUrl: "/",
+    },
+    {
+      positon: "Frontend-Developer Intern",
+      company: "UnityLabs.ai",
+      location: "Bangalore, INDIA",
+      duration: "Feb 2022 - May 2022",
+      resposibilities: [
+        {
+          data: "Developed new user-facing features using JavaScript Frameworks like ReactJs and NextJs",
+        },
+        {
+          data: "Build reusable components and front-end libraries for future use",
+        },
+        {
+          data: "Documented application changes and developing updates",
+        },
+      ],
+      companyUrl: "/",
+    },
+  ];
   return (
     <InternWrapper>
       <h1>Where I've Worked</h1>
-      <div className="para-wrap">
-        <h3>
-          Frontend-Developer Intern @{" "}
-          <a href="https://unitylabs.ai/">UnityLabs.ai</a>{" "}
-          <div className="date">Feb 2022 - May 2022</div>
-        </h3>
-        <p>
-          <ol className="list">
-            <li>
-              <span>
-                <ArrowRightIcon />
-              </span>
-              Developed a website using NextJs and ReactJs
-            </li>
-            <li>
-              <span>
-                <ArrowRightIcon />
-              </span>
-              Worked with a team of other talented UI/UX devs on the website
-            </li>
-            <li>
-              <span>
-                <ArrowRightIcon />
-              </span>
-              Managed the documentation for the website
-            </li>
-          </ol>
-        </p>
-        <Button
-          variant="outlined"
-          onClick={handleClick}
-          className="btn"
-          sx={styles}
-        >
-          <text className="menuTextbtn">CERTIFICATE</text>
-        </Button>
-      </div>
+      {data.map((ele, key) => (
+        <div className="para-wrap">
+          <h3>
+            {ele.positon} @ <a href={ele.companyUrl}>{ele.company}</a>{" "}
+            <div className="location">{ele.location}</div>
+            <div className="date">{ele.duration}</div>
+          </h3>
+          <p>
+            <ol className="list">
+              {ele.resposibilities.map((i) => (
+                <li>
+                  <span>
+                    <ArrowRightIcon />
+                  </span>
+                  {i.data}
+                </li>
+              ))}
+            </ol>
+          </p>
+          <Button
+            variant="outlined"
+            onClick={handleClick}
+            className="btn"
+            sx={styles}
+          >
+            <text className="menuTextbtn">CERTIFICATE</text>
+          </Button>
+        </div>
+      ))}
     </InternWrapper>
   );
 };
